8610a958dde15b99a195ba30180ed0abaaf04739
mcheviron <87418909+mcheviron@users.noreply.github.com>
2022-09-14 17:53:21 +0200

    Update netcat.py

--- netcat.py
+++ netcat.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
-import socket
 import shlex
+import socket
 import subprocess
 import sys
 import textwrap
@@ -10,11 +10,13 @@ import threading
 
 
 # This is the main drive for command execution in the tool
-def execute(cmd):
+def execute(cmd: str) -> str | None:
     cmd = cmd.strip()
     if not cmd:
         return
-    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    output = subprocess.check_output(
+        shlex.split(cmd), stderr=subprocess.STDOUT
+    )
     return output.decode()
 
 
@@ -63,7 +65,9 @@ class NetCat:
         self.socket.listen(5)
         while True:
             client_socket, _ = self.socket.accept()
-            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
+            client_thread = threading.Thread(
+                target=self.handle, args=(client_socket,)
+            )
             client_thread.start()
 
     # Depending on which of the other three options you choose, it'll be handled with this method
@@ -125,11 +129,21 @@ if __name__ == "__main__":
             """
             ),
         )
-        parser.add_argument("-c", "--command", action="store_true", help="command shell")
-        parser.add_argument("-e", "--execute", help="execute specified command")
-        parser.add_argument("-l", "--listen", action="store_true", help="listen")
-        parser.add_argument("-p", "--port", type=int, default=9999, help="specified port")
-        parser.add_argument("-t", "--target", default="192.168.1.203", help="specified IP")
+        parser.add_argument(
+            "-c", "--command", action="store_true", help="command shell"
+        )
+        parser.add_argument(
+            "-e", "--execute", help="execute specified command"
+        )
+        parser.add_argument(
+            "-l", "--listen", action="store_true", help="listen"
+        )
+        parser.add_argument(
+            "-p", "--port", type=int, default=9999, help="specified port"
+        )
+        parser.add_argument(
+            "-t", "--target", default="192.168.1.203", help="specified IP"
+        )
         parser.add_argument("-u", "--upload", help="upload file")
         args = parser.parse_args()
         if args.listen:
